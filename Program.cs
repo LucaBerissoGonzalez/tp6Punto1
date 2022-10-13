d7b361196e3e190931c859c6dff45dd7b4df3e86
lucaberissogonzalez <lucaberisso1@gmail.com>
2022-10-13 10:27:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace csharp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            //Console.ReadKey();
+            //Console.Clear();
+            Console.WriteLine("Preparacion de Cafe instantaneo");
+            Console.ReadKey();
+            Console.WriteLine("Paso 1: Calentar agua caliente");
+            Console.ReadKey();
+            Console.WriteLine("Paso 2: Echar azucar y cafe a una taza");
+            Console.ReadKey();
+            Console.WriteLine("Paso 3: Echar el agua caliente en la taza");
+            Console.ReadKey();
+            Console.WriteLine("Paso 4: Mezclar el agua");
+            Console.ReadKey();
+        }
+    }
+}
